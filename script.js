274945334d5ed15227a987870a862e3365e73962
harusame1018 <163406621+harusame1018@users.noreply.github.com>
2024-12-09 05:55:54 +0000

    change

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let form = document.getElementById("form");
-let mondaiform = form.quetion.value();
-mondaiform.addEventListener() 
+let mondai = [];
+let kotae = [];
+let question = document.getElementById("question");
+let anser = document.getElementById("anser");
+let sousin = document.getElementById("sousin");
+sousin.addEventListener("submit",function(e) {
+    mondai.push(question);
+    kotae.push(anser);
+    let bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    let blob = new Blob([bom, mondai],{type:"text/csv"});
+    let blob2 = new Blob([bom,kotae],{type:"text/csv"});
+    let link = document.createElement('a');
+    let link2 = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link2.href = URL.createObjectURL(blob2);
+    link.download = 'mondai.csv';
+    link2.download = "kotae.csv";
+    link.click();
+    link2.click();
+});
